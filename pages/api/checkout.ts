aab9f0be5161ba007559b3c29b5478a72cefaf33
ianteodoros <ianteodoros@zohomail.eu>
2025-06-17 23:59:35 +0300

    Update checkout.ts

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+apiVersion: '2024-04-10' as Stripe.LatestApiVersion,
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
